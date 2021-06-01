7fb6ab250c2b66f6c391c645825c5992cabb37c2
farkalit76 <30614406+farkalit76@users.noreply.github.com>
2021-06-01 16:16:01 +0530

    Use JWT security code
    Use JWT security code

---
+++ usman-spboot-jwt-call/src/main/java/com/farkalit/test/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,28 @@
+package com.farkalit.test.config;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+	private static final Logger LOG = LogManager.getLogger(JwtAuthenticationEntryPoint.class);
+
+	private static final long serialVersionUID = -7858869558953243875L;
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException {
+		LOG.info("commence.request:{}", request);
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+	}
+}
